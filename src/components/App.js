7a05dcb216c467e146f33e86e01fb06c28188850
Prajwal-Jadhav <jadhavv933@gmail.com>
2020-07-06 15:19:40 +0530

    added requireAuth higher order component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import { Route } from "react-router-dom";
 import Welcome from "./Welcome";
 import SignUp from "./auth/SignUp";
+import Feature from "./Feature";
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class App extends Component {
         <Header />
         <Route path="/" exact component={Welcome} />
         <Route path="/signup" component={SignUp} />
+        <Route path="/feature" component={Feature} />
       </div>
     );
   }
